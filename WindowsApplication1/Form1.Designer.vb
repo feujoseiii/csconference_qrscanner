8551276418e471abf40b98d306c2003cc0bf96d3
Jose Miranda <feu.joseiii@gmail.com>
2017-06-26 18:14:23 +0800

    Initial Commit

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainApplication
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.AName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Time = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.AName, Me.Time})
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.DataGridView1.Location = New System.Drawing.Point(558, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(706, 681)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(552, 681)
+        Me.Panel1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button1.Location = New System.Drawing.Point(0, 636)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(552, 45)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Confirm"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(0, 248)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(552, 433)
+        Me.TextBox1.TabIndex = 5
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(552, 248)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'AName
+        '
+        Me.AName.FillWeight = 50.0!
+        Me.AName.HeaderText = "Name"
+        Me.AName.Name = "AName"
+        Me.AName.ReadOnly = True
+        Me.AName.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.AName.Width = 5
+        '
+        'Time
+        '
+        Me.Time.FillWeight = 50.0!
+        Me.Time.HeaderText = "Time"
+        Me.Time.Name = "Time"
+        Me.Time.ReadOnly = True
+        Me.Time.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.Time.Width = 5
+        '
+        'MainApplication
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "MainApplication"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents PName As DataGridViewTextBoxColumn
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Time As DataGridViewTextBoxColumn
+    Friend WithEvents AName As DataGridViewTextBoxColumn
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports ZXing
+Imports ZXing.BarcodeReader
+Imports Emgu
+Imports Emgu.CV.Capture
+Imports System.Net
+Imports System.Web.Script.Serialization
+Imports Newtonsoft.Json
+Imports System.IO
+
+Public Class MainApplication
+
+    Dim thread As Threading.Thread
+    Dim camcapture As Emgu.CV.Capture
+    Dim decoder As ZXing.BarcodeReader
+    Dim image, copy As Bitmap
+    Dim result As Object
+
+    Dim resultString As String
+
+    Dim apifindclient As New WebClient
+    Dim apisetclient As New WebClient
+    Dim apifindresponse, apisetresponse As String
+
+    Dim isBusy As Boolean = False
+
+    Dim detailsurl As String = "http://localhost/feutechcs/public/getdetails/"
+    Dim setarrivalurl As String = "http://localhost/feutechcs/public/setarrival/"
+
+    Dim jss As New Newtonsoft.Json.JsonSerializer
+
+    'participant details
+    Dim fullname As String = ""
+    Dim timearrived As String = ""
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CheckForIllegalCrossThreadCalls = False
+        Me.StartPosition = FormStartPosition.WindowsDefaultLocation
+        Me.PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        Me.DataGridView1.Columns(0).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        Me.DataGridView1.Columns(1).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+
+        thread = New Threading.Thread(AddressOf startCapture)
+        thread.Start()
+        Timer1.Start()
+    End Sub
+
+    Private Sub startCapture()
+        camcapture = New CV.Capture(1)
+        While True
+            image = New Bitmap(camcapture.QueryFrame().Bitmap)
+            If image IsNot Nothing Then
+                PictureBox1.Image = image
+            End If
+        End While
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If thread.IsAlive Then
+            Capture = Nothing
+            thread.Abort()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If isBusy = False Then
+            If image IsNot Nothing Then
+                resultString = decodeImage(image)
+                If resultString IsNot Nothing Then
+                    resultString = resultString.Trim(" ")
+                    If resultString IsNot String.Empty Then
+                        findAttendee(resultString)
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If fullname IsNot String.Empty AndAlso timearrived IsNot String.Empty Then
+            TextBox1.Text = ""
+            DataGridView1.Rows.Add(fullname, timearrived)
+        End If
+    End Sub
+
+    Private Function decodeImage(b As Bitmap)
+        Dim o As Object
+        decoder = New ZXing.BarcodeReader
+        o = decoder.Decode(b)
+        If o IsNot Nothing Then
+            Return o.ToString
+        End If
+        Return Nothing
+    End Function
+
+    Private Sub findAttendee(decoded As String)
+        isBusy = True
+        Dim qr As String = decoded
+        apifindresponse = apifindclient.DownloadString(detailsurl + decoded)
+
+        Dim searchDictionary As Dictionary(Of String, String) = jss.Deserialize(
+            Of Dictionary(Of String, String))(New JsonTextReader(New StringReader(apifindresponse)))
+
+        If searchDictionary.ContainsKey("error") Then
+            MessageBox.Show(searchDictionary("error"), "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            apisetresponse = apifindclient.DownloadString(setarrivalurl + searchDictionary("id"))
+            Dim findDictionary As Dictionary(Of String, String) = jss.Deserialize(
+                Of Dictionary(Of String, String))(
+                New JsonTextReader(New StringReader(apisetresponse)))
+            If findDictionary.ContainsKey("error") Then
+                MessageBox.Show(findDictionary("error"), "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                If findDictionary("status") = "success" Then
+                    fullname = searchDictionary("last_name") + ", " +
+                                    searchDictionary("first_name") + " " +
+                                    searchDictionary("middle_name").ElementAt(0) + "."
+                    timearrived = DateTime.Now.ToString()
+                    Dim participant_type As String
+                    If searchDictionary("is_student") = "1" Then
+                        participant_type = "Student"
+                    Else
+                        participant_type = "Professional"
+                    End If
+                    TextBox1.AppendText("Name: " + fullname + Environment.NewLine)
+                        TextBox1.AppendText("Course: " + searchDictionary("course") + Environment.NewLine)
+                        TextBox1.AppendText("School: " + searchDictionary("school") + Environment.NewLine)
+                        TextBox1.AppendText("Email: " + searchDictionary("email") + Environment.NewLine)
+                        TextBox1.AppendText("Contact: " + searchDictionary("contact") + Environment.NewLine)
+                        TextBox1.AppendText("Date Registered: " + searchDictionary("created_at") + Environment.NewLine)
+                    TextBox1.AppendText("Participant Type: " + participant_type)
+
+                End If
+                End If
+        End If
+        isBusy = False
+    End Sub
+
+End Class
